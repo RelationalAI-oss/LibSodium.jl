83fe00dd2cd99eaa4b68ec3e668236155bf330c8
George Kollias <286415+geokollias@users.noreply.github.com>
2020-05-04 20:01:08 +0300

    Fix spacing

--- gen/generate.jl
+++ gen/generate.jl
@@ -12,16 +12,16 @@ const LIBSODIUM_DIR = dirname(dirname(m.libsodium_jll.libsodium_path))
 const LIBSODIUM_INCLUDE = joinpath(LIBSODIUM_DIR, "include", "sodium")
 
 const HEADER_FNAMES = ["core.h",
-		       "export.h",
-		       "crypto_hash_sha512.h",
-		       "crypto_sign_ed25519.h"]
+                       "export.h",
+                       "crypto_hash_sha512.h",
+                       "crypto_sign_ed25519.h"]
 const HEADER_PATHS = map(hn -> joinpath(LIBSODIUM_INCLUDE, hn) |> normpath,
-			 HEADER_FNAMES)
+                         HEADER_FNAMES)
 
 const IGNORE_DEFINITIONS = ["crypto_sign_ed25519_MESSAGEBYTES_MAX",
-			    "crypto_sign_ed25519_open"]
+                            "crypto_sign_ed25519_open"]
 function cursor_wrapped(cursor_name, cursor)
-  any(map(def -> startswith(cursor_name, def), IGNORE_DEFINITIONS)) ? false : true
+    any(map(def -> startswith(cursor_name, def), IGNORE_DEFINITIONS)) ? false : true
 end
 
 const OUTPUT_DIR = abspath(@__DIR__, "out")
@@ -33,7 +33,7 @@ wc = init(; headers = HEADER_PATHS,
             clang_args = ["-I", joinpath(LIBSODIUM_INCLUDE, "..")],
             header_wrapped = (root, current)->root == current,
             header_library = x->"libsodium",
-	    cursor_wrapped = cursor_wrapped,
+	        cursor_wrapped = cursor_wrapped,
             clang_diagnostics = true,
             )
 
